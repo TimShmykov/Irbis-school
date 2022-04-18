55758c9ba6b3ce281e8e9b791a74f2130661eb76
TimShmykov <shmyk.ta@gmail.com>
2022-04-18 15:15:59 +0400

    Урок 2 в Ирбисе

---
+++ src/Irbis_Lesson_2/Lesson_2.java
@@ -0,0 +1,40 @@
+package Irbis_Lesson_2;
+
+public class Lesson_2 {
+    public static void main(String[] args) { // цикл for
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(i); // массивы ---> далее
+//        }
+//
+//        int[] mass = new int[3]; // вид массива (тип [] имя = new [i];
+//        mass[0] = 1;
+//        mass[1] = 2;
+//        mass[2] = 3;
+//
+//        int[] mass2 = {1, 2, 3};
+//
+//        for (int i = 0; i < mass.length; i++) {
+//            mass[i] = mass[i] + mass2[i]; // объявляем что массив первый становиться новым новым массивом
+//            System.out.println(mass[i]);
+//        }
+//
+//        for (int i : mass) { // for each как дублер верхнего
+//            System.out.println(i);
+
+            int[][] mass3 = new int[3][3];
+            int[][] mass4 = {{1, 2, 3}, {1, 2, 3}}; // тоже самое что и вверх, но с конкретными данными а в верху просто 3 на 3
+            int[][] mass5 = new int[3][3];
+
+
+            for (int y = 0; y < mass3.length; y++) {
+                for (int j = 0; j < mass5.length; j++) {
+                    mass3[y][j] = 0;
+                    System.out.print(mass3[y][j] + " ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+
+
