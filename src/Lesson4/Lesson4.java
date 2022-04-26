ccc818e8532959c2c37c4356f3bea762d8a10ae2
TimShmykov <shmyk.ta@gmail.com>
2022-04-26 12:14:19 +0400

    добавил 4 урок

---
+++ src/Lesson4/Lesson4.java
@@ -0,0 +1,18 @@
+public class Lesson4 {
+
+    public int sum(int x, int y) {
+        return x + y;
+
+
+    }
+
+    public static void main(String[] args) {
+        String str; // методы стринг
+        str.substring( int  0; int  5);
+        String ss = " firstname";
+        str.join(" ", firstname, secondname); // - создает пробелы
+    }
+
+
+    
+}
